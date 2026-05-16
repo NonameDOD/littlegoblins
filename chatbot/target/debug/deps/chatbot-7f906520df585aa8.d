a407dbad47045db4cbc3845977b6a234ed8dad89
Haraszti Mihály <harasztimisi.rav@gmail.com>
2026-05-16 13:17:35 +0200

    chatbot init

---
+++ chatbot/target/debug/deps/chatbot-7f906520df585aa8.d
@@ -0,0 +1,10 @@
+/media/ADAT/Programok/html/littlegoblins/chatbot/target/debug/deps/chatbot-7f906520df585aa8.d: src/lib.rs src/chatbot/mod.rs Cargo.toml
+
+/media/ADAT/Programok/html/littlegoblins/chatbot/target/debug/deps/libchatbot-7f906520df585aa8.rmeta: src/lib.rs src/chatbot/mod.rs Cargo.toml
+
+src/lib.rs:
+src/chatbot/mod.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ chatbot/target/debug/deps/chatbot-e567d8b3a8a22f5a.d
@@ -0,0 +1,10 @@
+/media/ADAT/Programok/html/littlegoblins/chatbot/target/debug/deps/chatbot-e567d8b3a8a22f5a.d: src/lib.rs src/chatbot/mod.rs Cargo.toml
+
+/media/ADAT/Programok/html/littlegoblins/chatbot/target/debug/deps/libchatbot-e567d8b3a8a22f5a.rmeta: src/lib.rs src/chatbot/mod.rs Cargo.toml
+
+src/lib.rs:
+src/chatbot/mod.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
