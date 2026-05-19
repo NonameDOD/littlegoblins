dcbc9d6ba51ed27f3e1f009cff55171983744120
Noname <83223643+NonameDOD@users.noreply.github.com>
2026-05-19 13:04:25 +0200

    Visual studio code moment
    Resolved merge conflict by updating image URLs to use relative paths.

--- assets/js/gallery_cat_list.js
+++ assets/js/gallery_cat_list.js
@@ -1,17 +1,8 @@
 export const gallery_cat_list = [
-<<<<<<< HEAD
     { name: "Lara", url: "../../assets/gallery/gallery_lara_1.jpg" },
     { name: "Lara", url: "../../assets/gallery/gallery_lara_2.jpg" },
     { name: "Lara", url: "../../assets/gallery/gallery_lara_3.jpg" },
     { name: "Osiris", url: "../../assets/gallery/gallery_osi_1.jpg" },
     { name: "Osiris", url: "../../assets/gallery/gallery_osi_2.jpg" },
     { name: "Osiris", url: "../../assets/gallery/gallery_osi_3.jpg" },
-=======
-    { name: "Lara", url: "./assets/gallery/gallery_lara_1.jpg" },
-    { name: "Lara", url: "./assets/gallery/gallery_lara_2.jpg" },
-    { name: "Lara", url: "./assets/gallery/gallery_lara_3.jpg" },
-    { name: "Osiris", url: "./assets/gallery/gallery_osi_1.jpg" },
-    { name: "Osiris", url: "./assets/gallery/gallery_osi_2.jpg" },
-    { name: "Osiris", url: "./assets/gallery/gallery_osi_3.jpg" },
->>>>>>> 4ef5d1111ae0954a2e89b3a7d3140977cb22953b
 ];
