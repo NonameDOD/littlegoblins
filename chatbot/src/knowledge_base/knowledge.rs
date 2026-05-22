fb2198b1a10d48b51a716f25f87db2309db5b031
Haraszti Mihály <harasztimisi.rav@gmail.com>
2026-05-22 17:31:53 +0200

    Knowleges and Fingerprint progress

--- chatbot/src/knowledge_base/knowledge.rs
+++ chatbot/src/knowledge_base/knowledge.rs
@@ -3,9 +3,37 @@ pub(super) struct Knowledge {
     knowledge: String,
 }
 
-type Fingerprint = Box<[SubFingerprint]>;
+// fp
+struct Fingerprint {
+    sub_fingerprints: Box<[SubFingerprint]>,
+    count: usize,
+}
 
+// v2-ben majd meg csinálom egyenlőre működjön valami
+// sfp
 struct SubFingerprint {
     fingerprint: Box<[String]>,
     count: usize,
+}
+
+impl Knowledge {
+    //Konstruktor
+    fn new(text: String) -> Self {
+        let words: Vec<&str> = text.rsplit(" ").collect();
+        let pre_fingerprint: Vec<Fingerprint>;
+        todo!()
+    }
+}
+
+impl Fingerprint {
+    fn new(word: &str) -> Self {
+        todo!();
+    }
+}
+
+impl SubFingerprint {
+    //talán krakter lista lenne a jód döntés
+    fn new(word: [u8]) -> Self {
+        todo!();
+    }
 }
