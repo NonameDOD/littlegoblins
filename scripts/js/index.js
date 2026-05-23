1d915091fd6c6a2c0d02cec9ba83e8082c0383b7
Fóti Dávid <fotidavid05@gmail.com>
2026-05-23 09:51:49 +0200

    Memóriajáték befejezve

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,11 +1,19 @@
 import { gallery_cat_list } from "./gallery_cat_list.js";
-import Cat_Card from "./Cat_Card.js";
 import Gallery from "./Gallery.js";
-import Memory_Renderer from "./Memory_Renderer.js";
+import Memory_Deck from "./memory_deck.js";
 import { memory_cat_list } from "./memory_cat_list.js";
-
+ 
 const galleryContainer = document.getElementById("gallery");
-const memoryContainer = document.getElementById("memory_game");
-
-new Gallery(gallery_cat_list,galleryContainer);
-new Memory_Renderer(memory_cat_list,memoryContainer);
+const memoryContainer  = document.getElementById("memory_game");
+const startBtn         = memoryContainer.querySelector("button");
+ 
+// Galéria – változatlan
+new Gallery(gallery_cat_list, galleryContainer);
+ 
+// Memóriajáték indítása a gombra kattintva
+startBtn.addEventListener("click", () => {
+  // Gomb eltávolítása, majd játék létrehozása
+  startBtn.remove();
+  new Memory_Deck(memory_cat_list, memoryContainer);
+});
+ 
--- scripts/js/memory_cat_list.js
+++ scripts/js/memory_cat_list.js
@@ -1,8 +1,8 @@
 export const memory_cat_list = [
-    { name: "Lara", url: "../../assets/gallery/gallery_lara_1.jpg" },
-    { name: "Lara", url: "../../assets/gallery/gallery_lara_2.jpg" },
-    { name: "Lara", url: "../../assets/gallery/gallery_lara_3.jpg" },
-    { name: "Osiris", url: "../../assets/gallery/gallery_osi_1.jpg" },
-    { name: "Osiris", url: "../../assets/gallery/gallery_osi_2.jpg" },
-    { name: "Osiris", url: "../../assets/gallery/gallery_osi_3.jpg" },
+    { name: "Lara_1", url: "../../assets/gallery/gallery_lara_1.jpg" },
+    { name: "Lara_2", url: "../../assets/gallery/gallery_lara_2.jpg" },
+    { name: "Lara_3", url: "../../assets/gallery/gallery_lara_3.jpg" },
+    { name: "Osiris_1", url: "../../assets/gallery/gallery_osi_1.jpg" },
+    { name: "Osiris_2", url: "../../assets/gallery/gallery_osi_2.jpg" },
+    { name: "Osiris_3", url: "../../assets/gallery/gallery_osi_3.jpg" },
 ];
