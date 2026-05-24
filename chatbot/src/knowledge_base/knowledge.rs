b431ac0155fe0f6699bce8a91b4b9f86be5e6c60
Haraszti Mihály <harasztimisi.rav@gmail.com>
2026-05-24 08:43:41 +0200

    Revert "Knowleges and Fingerprint progress"
    This reverts commit fb2198b1a10d48b51a716f25f87db2309db5b031.

--- chatbot/src/knowledge_base/knowledge.rs
+++ chatbot/src/knowledge_base/knowledge.rs
@@ -3,37 +3,9 @@ pub(super) struct Knowledge {
     knowledge: String,
 }
 
-// fp
-struct Fingerprint {
-    sub_fingerprints: Box<[SubFingerprint]>,
-    count: usize,
-}
+type Fingerprint = Box<[SubFingerprint]>;
 
-// v2-ben majd meg csinálom egyenlőre működjön valami
-// sfp
 struct SubFingerprint {
     fingerprint: Box<[String]>,
     count: usize,
-}
-
-impl Knowledge {
-    //Konstruktor
-    fn new(text: String) -> Self {
-        let words: Vec<&str> = text.rsplit(" ").collect();
-        let pre_fingerprint: Vec<Fingerprint>;
-        todo!()
-    }
-}
-
-impl Fingerprint {
-    fn new(word: &str) -> Self {
-        todo!();
-    }
-}
-
-impl SubFingerprint {
-    //talán krakter lista lenne a jód döntés
-    fn new(word: [u8]) -> Self {
-        todo!();
-    }
 }
