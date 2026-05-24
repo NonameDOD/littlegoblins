e6bf29633770c83682b776ead3ad7efdeeb7713f
Haraszti Mihály <harasztimisi.rav@gmail.com>
2026-05-24 10:37:43 +0200

    Proggres on Knowledge and a bit of mess up

--- chatbot/src/knowledge_base/fingerprint_traits.rs
+++ chatbot/src/knowledge_base/fingerprint_traits.rs
@@ -1,2 +1,10 @@
-pub trait Fingerprint {}
-pub trait SubFingerprint {}
+pub(super) trait Fingerprinteable {
+    fn evaluate<T: Fingerprinteable>(&self, other: T) -> f32;
+    fn fsize(&self) -> usize;
+    fn fingerprints<T: Fingerprinteable>(&self) -> Result<&Box<[T]>, ()> {
+        Err(())
+    }
+    fn sub_fingerprints<T: Fingerprinteable>(&self) -> Result<&Box<[String]>, ()> {
+        Err(())
+    }
+}
--- chatbot/src/knowledge_base/knowledge.rs
+++ chatbot/src/knowledge_base/knowledge.rs
@@ -1,39 +1,94 @@
+use crate::knowledge_base::fingerprint_traits::Fingerprinteable;
+
 pub(super) struct Knowledge {
     fingerprint: Fingerprint,
     knowledge: String,
 }
 
+impl Knowledge {
+    fn new(text: String) -> Self {
+        Knowledge {
+            fingerprint: Fingerprint::new(&text),
+            knowledge: text,
+        }
+    }
+}
+
+// --- Fingerprint
+
+// ez a "szöveg"
 // fp
 struct Fingerprint {
-    sub_fingerprints: Box<[SubFingerprint]>,
-    count: usize,
+    fingerprint: Box<[SubFingerprint]>,
 }
 
-// v2-ben majd meg csinálom egyenlőre működjön valami
-// sfp
-struct SubFingerprint {
-    fingerprint: Box<[String]>,
-    count: usize,
-}
-
-impl Knowledge {
-    //Konstruktor
-    fn new(text: String) -> Self {
+impl Fingerprint {
+    fn new(text: &str) -> Self {
         let words: Vec<&str> = text.rsplit(" ").collect();
-        let pre_fingerprint: Vec<Fingerprint>;
+        let mut pre_SubFingerprint: Vec<Fingerprint> = Vec::new();
+        for word in words {}
         todo!()
     }
 }
 
-impl Fingerprint {
-    fn new(word: &str) -> Self {
+impl Fingerprinteable for Fingerprint {
+    // A "szövegnek" a SubFingerprint-eit evaluate-eljük
+    fn evaluate<T: Fingerprinteable>(&self, other: T) -> f32 {
+        let mut likeliness: f32 = 0.0;
+        let mut n_of: usize = 0;
+        for this in self.fingerprint {
+            for that in other.fingerprints() {}
+        }
+        todo!()
+    }
+    fn fsize(&self) -> usize {
+        self.fingerprint.len()
+    }
+    // ha roszz a tipus -> Err
+    fn fingerprints<T: Fingerprinteable>(&self) -> Result<&Box<[T]>, ()> {
+        Ok(&self.fingerprint)
+    }
+}
+
+// --- SubFingerprint
+
+// v2-ben majd meg csinálom egyenlőre működjön valami
+// ezek a "szavak"
+// sfp
+struct SubFingerprint {
+    pub fingerprint: Box<[String]>,
+    pub count: usize,
+}
+
+impl SubFingerprint {
+    //talán krakter lista lenne a jó döntés
+    fn new(word: Box<[u8]>) -> Self {
         todo!();
     }
 }
 
-impl SubFingerprint {
-    //talán krakter lista lenne a jód döntés
-    fn new(word: [u8]) -> Self {
+impl Fingerprinteable for SubFingerprint {
+    // A "szavakat" evaluate-eljük
+    fn evaluate<T: Fingerprinteable>(&self, other: T) -> f32 {
         todo!();
+    }
+    fn fsize(&self) -> usize {
+        self.fingerprint.len()
+    }
+    // ha roszz a tipus -> Err
+    fn sub_fingerprints<T: Fingerprinteable>(&self) -> Result<&Box<[String]>, ()> {
+        Ok(&self.fingerprint)
+    }
+}
+
+// --- statikus funkciók
+
+// mindig a nagyban keressük a kicsit
+// ez kész
+pub(super) fn evaluate<T: Fingerprinteable>(this: T, that: T) -> f32 {
+    if this.fsize() > that.fsize() {
+        this.evaluate(that)
+    } else {
+        that.evaluate(this)
     }
 }
