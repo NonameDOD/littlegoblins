e52636a4fd5c8f85ce308c509586f27a61138f21
Ravasszdi <harasztimisi.rav@gmail.com>
2026-05-27 15:22:27 +0200

    HM big teszt

--- cypress/e2e/HMTeszt.cy.js
+++ cypress/e2e/HMTeszt.cy.js
@@ -4,7 +4,12 @@ describe("template spec", () => {
   });
   it("passes", () => {
     cy.visit("https://nonamedod.github.io/littlegoblins/test/almok_test.html");
-cy.get(':nth-child(1) > .card').click();
-cy.get(':nth-child(7) > .card').click().should('have.class', 'border-success');
+    let offset = 6;
+    let sorRend = [1,1+offset,2,2+offset,3,3+offset,4,4+offset,5,5+offset,6,6+offset];
+    //cy.get('.card').eq(1).click();
+    //cy.get('.card').eq(7).click().should('have.class', 'border-success');
+    for(let i = 1;i<sorRend.length;i++){
+      cy.get('.card').eq(sorRend[i]).click();
+    }
   });
 });
