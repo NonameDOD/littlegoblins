449dbe985df982565a07db1ae098ed867ee92d46
Haraszti Mihály <harasztimisi.rav@gmail.com>
2026-05-27 20:47:25 +0200

    cypress teszt tovább dolgozás

--- cypress/e2e/HMTeszt.cy.js
+++ cypress/e2e/HMTeszt.cy.js
@@ -1,15 +1,22 @@
 describe("template spec", () => {
-  it("passes", () => {
+  it("Oldal látogatás", () => {
     cy.visit("https://nonamedod.github.io/littlegoblins/test/almok_test.html");
   });
-  it("passes", () => {
+  it("Memoria játék funkcionalitás (nem random)", () => {
     cy.visit("https://nonamedod.github.io/littlegoblins/test/almok_test.html");
     let offset = 6;
-    let sorRend = [1,1+offset,2,2+offset,3,3+offset,4,4+offset,5,5+offset,6,6+offset];
+    let sorRend = [0, 0 + offset, 1, 1 + offset, 2, 2 + offset, 3, 3 + offset, 4, 4 + offset, 5, 5 + offset];
     //cy.get('.card').eq(1).click();
     //cy.get('.card').eq(7).click().should('have.class', 'border-success');
-    for(let i = 1;i<sorRend.length;i++){
+    for (let i = 0; i < sorRend.length; i++) {
       cy.get('.card').eq(sorRend[i]).click();
     }
+    for (let i = 0; i < sorRend.length; i++) {
+      cy.get('.card').eq(sorRend[i]).should('have.class', 'border-success');
+    }
+    cy.get('.col-12').should('be.visible');
   });
 });
+
+
+// hasznos oldal [cypress cheatsheet](https://docs.w3cub.com/cypress/)
