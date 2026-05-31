5fdcdd4a6a74c9217dcaf5743fd6053efc001598
Fóti Dávid <fotidavid05@gmail.com>
2026-05-31 11:47:56 +0200

    Kész szöveg, memória kártya lista képek feltöltve 1. alom macskáival

--- src/js/memory_cat_list.js
+++ src/js/memory_cat_list.js
@@ -1,8 +1,10 @@
 export const memory_cat_list = [
-    { name: "Lara_1", url: "../../assets/gallery/gallery_lara_1.jpg" },
-    { name: "Lara_2", url: "../../assets/gallery/gallery_lara_2.jpg" },
-    { name: "Lara_3", url: "../../assets/gallery/gallery_lara_3.jpg" },
-    { name: "Osiris_1", url: "../../assets/gallery/gallery_osi_1.jpg" },
-    { name: "Osiris_2", url: "../../assets/gallery/gallery_osi_2.jpg" },
-    { name: "Osiris_3", url: "../../assets/gallery/gallery_osi_3.jpg" },
+    { name: "Lara", url: "../../assets/memory_game/litter_1/lara.jpg" },
+    { name: "Osiris", url: "../../assets/memory_game/litter_1/osiris.jpg" },
+    { name: "Alex", url: "../../assets/memory_game/litter_1/alex.jpg" },
+    { name: "Alfi", url: "../../assets/memory_game/litter_1/alfi.jpg" },
+    { name: "Álmos", url: "../../assets/memory_game/litter_1/almos.jpg" },
+    { name: "Amy", url: "../../assets/memory_game/litter_1/amy.jpg" },
+    { name: "Andrew", url: "../../assets/memory_game/litter_1/andrew.jpg" },
+    { name: "Artúr", url: "../../assets/memory_game/litter_1/artur.jpg" }
 ];
